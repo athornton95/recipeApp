8b0a6870588c352e2328fd10be3d5830b0d3bba2
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-04-25 16:37:08 -0600

    eod thursday commit

--- models/Recipe.js
+++ models/Recipe.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const recipeSchema = new mongoose.Schema({
     title: {type: String, required: true},
     img: {type: String, default: 'https://aucdn.ar-cdn.com/recipes/xlarge/default.jpg'},
-    body: {type: String, required: true}
+    body: {type: String, required: true},
+    user: {type: String, required: true}
 })
 
 
