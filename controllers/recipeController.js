189de4e5d4b3e9dc69736c70dc2ff419efebe286
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-04-25 12:56:35 -0600

    recipe show route working with login permissions

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -3,7 +3,7 @@
  const Recipe = require('../models/Recipe');
  const User = require('../models/User');
  
- 
+ //INDEX route:
  router.get('/', (req, res) => {
      Recipe.find({}, (err, recipesOnTheDatabase) => {
          if(err){
@@ -11,25 +11,33 @@
              res.send(err);
          } else {
              res.render('recipes/index.ejs', {
-                recipesOnTheTemplate: recipesOnTheDatabase
+                recipesOnTheTemplate: recipesOnTheDatabase,
+                logged: req.session.logged
              })
          }
      })
  })
  
+ //NEW route:
  router.get('/new', (req, res) => {
      res.render('recipes/new.ejs', {
      });
  })
 
-
+//SHOW route:
  router.get('/:id', async (req, res) => {
+     console.log(req.session)
      try {
                const recipeFromTheDatabase = await Recipe.findById(req.params.id);
                 const user = await User.findOne({"recipes": req.params.id})
+                
+ 
                 res.render('recipes/show.ejs', {
                     recipeOnTheTemplate: recipeFromTheDatabase,
-                    user: user
+                    user: user,
+                    logged: req.session.logged,
+                    userOnTheTemplate: user,
+                    sessionId: req.session.usersDbId
                 })
      } catch(err){
          console.log(err);
@@ -37,6 +45,7 @@
      }
  })
 
+ //CREATE route
  router.post('/', async (req, res) => {
     try{
        const newRecipe = await Recipe.create(req.body);
@@ -50,6 +59,7 @@
     }
 })
 
+//EDIT route
 router.get('/:id/edit', async (req, res) => {
     try{
         const foundRecipe = await Recipe.findById(req.params.id)
@@ -64,9 +74,25 @@ router.get('/:id/edit', async (req, res) => {
     }
 })
 
+
+//UPDATE route
 router.put('/:id', async (req, res) => {
     try{
         const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.redirect('/recipes')
+
+    }catch(err){
+        res.send(err)
+    }
+})
+
+//DELETE route:
+router.delete('/:id', async (req, res) => {
+    try{
+        const deletedRecipe = await Recipe.findByIdAndDelete(req.params.id)
+        const foundUser = await User.findOne({'recipes': req.params.id})
+        foundUser.recipes.remove(req.params.id)
+        const savedUser = await foundUser.save()
         res.redirect('/recipes')
 
     }catch(err){
