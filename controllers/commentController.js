690dc3dc92ea829ad2a18ff17b637c42d9f4fb18
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-04-29 16:56:04 -0600

    can delete comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,27 +1,24 @@
-const express = require('express');
- const router = express.Router();
- const Recipe = require('../models/Recipe');
- const User = require('../models/User');
- const Comment = require('../models/Comment');
+// const express = require('express');
+//  const router = express.Router();
+//  const Recipe = require('../models/Recipe');
+//  const User = require('../models/User');
+//  const Comment = require('../models/Comment');
 
 
-router.post('/:id', async (req, res) => {
-const newComment = await Comment.create(req.body);
-const foundRecipe = await Recipe.findOne({'._id': req.params.id})
-foundRecipe.comments.push(newComment._id);
-const allComments = await Comment.find({'recipe': req.params.id})
-res.render('recipes/show.ejs', {
-    recipeOnTheTemplate: recipeFromTheDatabase,
-    commentsOnTheTemplate: allComments,
-    user: user,
-    logged: req.session.logged,
-    sessionId: req.session.usersDbId,
-    username: req.session.username
-})
-})
-
-
-
+// router.post('/:id', async (req, res) => {
+// const newComment = await Comment.create(req.body);
+// const foundRecipe = await Recipe.findOne({'._id': req.params.id});
+// foundRecipe.comments.push(newComment._id);
+// const allComments = await Comment.find({'recipe': req.params.id});
+// res.render('recipes/show.ejs', {
+//     recipeOnTheTemplate: recipeFromTheDatabase,
+//     commentsOnTheTemplate: allComments,
+//     user: user,
+//     logged: req.session.logged,
+//     sessionId: req.session.usersDbId,
+//     username: req.session.username
+// })
+// })
 
 
 
@@ -35,4 +32,7 @@ res.render('recipes/show.ejs', {
 
 
 
- module.exports = router;
+
+
+
+//  module.exports = router;
--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -9,7 +9,7 @@
 router.get('/',  (req, res) => {
     const query = {};
     if(req.query.category){query.category = req.query.category};
-    console.log(query);
+    // console.log(query);
 
     Recipe.find(query,(err, recipesOnTheDatabase) => {
         if(err){
@@ -38,14 +38,14 @@ router.get('/',  (req, res) => {
 
 //SHOW route:
  router.get('/:id', async (req, res) => {
-     console.log(req.session)
+    //  console.log(req.session)
      try {
                const recipeFromTheDatabase = await Recipe.findById(req.params.id);
                 const user = await User.findOne({"recipes": req.params.id})
                 const allComments = await Comment.find({'recipe': req.params.id});
                 
-                console.log(user)
-                console.log()
+                // console.log(user)
+                // console.log()
                 res.render('recipes/show.ejs', {
                     recipeOnTheTemplate: recipeFromTheDatabase,
                     user: user,
@@ -75,7 +75,7 @@ router.get('/',  (req, res) => {
         console.log(foundUser)
         foundUser.recipes.push(newRecipe)
         const savedUser = await foundUser.save()
-        console.log(req.session.usersDbId, 'text comment')
+        // console.log(req.session.usersDbId, 'text comment')
         res.redirect('/recipes')
     }catch(err){
         console.log(err);
@@ -95,7 +95,7 @@ router.post('/:id', async (req, res) => {
         foundRecipe.comments.push(newComment._id);
         const savedRecipe = await foundRecipe.save();
         const allComments = await Comment.find({'recipe': req.params.id});
-        console.log(savedComment);
+        // console.log(savedComment);
         res.render('recipes/show.ejs', {
             recipeOnTheTemplate: foundRecipe,
             commentsOnTheTemplate: allComments,
@@ -111,17 +111,17 @@ router.post('/:id', async (req, res) => {
 })
 
 
-//CATEGORY route
-router.get('/category/:type', (req, res) => {
-    console.log(req.params.type);
-})
+// //CATEGORY route
+// router.get('/category/:type', (req, res) => {
+//     console.log(req.params.type);
+// })
 
 //EDIT route
 router.get('/:id/edit', async (req, res) => {
     try{
         const foundRecipe = await Recipe.findById(req.params.id)
         const foundUser = await User.findOne({'recipes': req.params.id})
-        console.log(foundRecipe);
+        // console.log(foundRecipe);
         res.render('recipes/edit.ejs', {
             recipeOnTheTemplate: foundRecipe,
             userOnTheTemplate: foundUser,
@@ -159,6 +159,39 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
+router.delete('/:id/:comment', async (req, res) => {
+    console.log('hit this route');
+        const deletedComment = await Comment.findByIdAndDelete(req.params.comment);
+        const foundRecipe = await Recipe.findById(req.params.id);
+        foundRecipe.comments.remove(req.params.comment);
+        const savedRecipe = await foundRecipe.save();
+        console.log(savedRecipe);
+
+
+
+
+
+
+
+    // const foundRecipe = await Recipe.findOne({'comments': req.params.id})
+    // const deletedComment = await Comment.findByIdAndDelete(req.params.id)
+    // console.log(foundRecipe);
+    // console.log(foundRecipe.comments);
+    // foundRecipe.comments.remove(req.params.comment);
+    // const savedRecipe = await foundRecipe.save();
+    // console.log(savedRecipe);
+    const user = await User.findOne({"recipes": req.params.id})
+    // // const foundRecipe = await Recipe.findById(req.params.id);
+    const allComments = await Comment.find({'recipe': req.params.id});
+    res.render('recipes/show.ejs', {
+        recipeOnTheTemplate: foundRecipe,
+        commentsOnTheTemplate: allComments,
+        user: user,
+        logged: req.session.logged,
+        sessionId: req.session.usersDbId,
+        username: req.session.username
+})
+})
 
 
  module.exports = router;
